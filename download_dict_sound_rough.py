f06219693f81e2a3d29d7e7ccb4389814dae61b6
Tim Leverentz <timlev@gmail.com>
2016-09-21 10:54:26 -0500

    added 'the crab'

--- download_dict_sound_rough.py
+++ download_dict_sound_rough.py
@@ -1,8 +1,9 @@
 import os, glob, urllib
 import download_dict_sound
 
+unable_to_download = []
 
-def dictionary_rough_search(word, directory="./newdownloads/"):
+def dictionary_rough_search(word, directory="./sounds/"):
     #if check_downloaded_word(word, directory):
         #return
     base = "http://dictionary.cambridge.org/us/dictionary/american-english/"
@@ -21,8 +22,11 @@ def dictionary_rough_search(word, directory="./newdownloads/"):
     # print "Found alternate source at", query2
     mp3source = ""
     for line in response:
-        if "sound audio_play_button pron-icon us" in line and ".mp3" in line:
-            # print line
+        #print line
+        if ".mp3" in line and "American" in line:
+        #if 'title="' + word + ':' in line and ".mp3" in line and "American" in line:
+        #if "sound audio_play_button pron-icon us" in line and ".mp3" in line:
+            print line
             start = line.find("data-src-mp3=") + len("data-src-mp3=") + 1
             end = line.find(".mp3") + len(".mp3")
             mp3source = line[start:end]
@@ -37,6 +41,7 @@ def dictionary_rough_search(word, directory="./newdownloads/"):
         ofp.close()
     except:
         print "Could not download:", word
+        unable_to_download.append(word)
 
 def download(word, directory="./"):
     if check_downloaded_word(word, directory):
@@ -72,48 +77,3 @@ def download(word, directory="./"):
     except:
         print "Could not download:", word
 
-def download_wiktionary(word, directory="./newdownloads/"):
-    base = "https://en.wiktionary.org/wiki/"
-    query = base + word
-    
-    handler = urllib.urlopen(query)
-    response = handler.readlines()
-    oggsource = ""
-    for line in response:
-        if "src=" in line and "_us_" in line and ".ogg" in line:
-            start = line.find("upload.wikimedia")
-            end = line.find(".ogg") + len(".ogg")
-            oggsource = "https://" + line[start:end]
-            print oggsource
-            break
-    print "Downloading", word, "to:", os.path.join(directory, word + ".ogg")
-    try:
-        getogg = urllib.urlopen(oggsource)
-        ofp = open(os.path.join(directory, word + ".ogg"),'wb')
-        ofp.write(getogg.read())
-        ofp.close()
-    except:
-        print "Could not download:", word
-
-def convert_ogg_to_mp3(oggfile, remove_ogg = False):
-    oggpath = os.path.abspath(oggfile)
-    ogg_dir = os.path.dirname(oggfile)
-    oggfile = os.path.basename(oggfile)
-    mp3file = oggfile.replace(".ogg",".mp3")
-    mp3path = oggpath.replace(".ogg",".mp3")
-    os.system('avconv -i "' + oggpath + '" "' + mp3path + '"')
-    if remove_ogg:
-        os.remove(oggpath)
-    return mp3path
-
-
-if __name__ == "__main__":
-    wordlist = ["looks","birds","sharks","tim","rebecca","monkeys","minnesota","penguins","says","joan","bats","names","jenn","goats","animals","teachers","thirtytwo","roseville","okay","students","seals"]
-    
-    for word in wordlist:
-        download_wiktionary(word,"./sounds/")
-        try:
-            convert_ogg_to_mp3(os.path.join(os.path.relpath("./sounds/"), word + ".ogg"), remove_ogg = True)
-        except:
-            print "Could't convert", word
-            
